bed4546684e40bd2e865b16ed2d0a58afb0fe4ed
Precha Chainara <143707409+Precha-chang@users.noreply.github.com>
2024-07-10 10:36:03 +0700

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -55,71 +57,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 30,
+              horizontal: 10,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            color: Color.fromARGB(255, 4, 3, 3),
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.settings,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Icon(
+                      Icons.cabin,
+                      color: Colors.white,
+                    ),
+                     SizedBox(
+                      width: 20,
+                    ),
+                    Icon(
+                      Icons.message,
+                      color: Colors.white,
+                    ),
+                     SizedBox(
+                      width: 20,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                   ClipOval(
+                    child:  Image.network('https://scontent-bkk1-1.xx.fbcdn.net/v/t39.30808-1/317232347_3206733259590165_4537751438105400688_n.jpg?stp=dst-jpg_p160x160&_nc_cat=107&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=C-dUkHIkPT0Q7kNvgHZP1--&_nc_ht=scontent-bkk1-1.xx&oh=00_AYB-gIxrp2VqqLru3wclpIz2CYVhg6uH1XhZezUgAPt3mw&oe=6692DCF8',
+                    width: 100,
+                    height: 100,
+                    )
+                   ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                   const Column(
+                    children: [
+                      Text("Precha Chainara",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                      ),
+                      Text("JK-CHanG",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "ผู้ติดตาม 200k | กำลังติดตาม 0",
+                            style: TextStyle(color: Color.fromARGB(255, 244, 242, 242),)
+                          )
+                        ],
+                      )
+                    ],
+                   )
+                  ],
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 5,
+              horizontal: 10,
+            ),
+            color: Color.fromARGB(255, 232, 5, 5),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 30,
+              horizontal: 10,
+            ),
+            color: Color.fromARGB(255, 4, 3, 3),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
